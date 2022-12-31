9f1c695ee8ca9b4ca937383b20ffa808cda9c921
Tariq Soliman <tariqsoliman2000@gmail.com>
2022-12-31 13:41:19 +0800

    Updated keymaps

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -1,7 +1,79 @@
 -- Keymaps for vim
 
+-- for default keybindings see :help index
+-- for plugin/user keybindings see :map and :map!
+-- to see where the keybindings are defined see :verbose-map
+-- for more details see :help map
+
 local opts = { noremap = true, silent = true }
+local termopts = { silent = true }
 local keymap = vim.api.nvim_set_keymap
 
+-- Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader =  " "
+vim.g.maplocalleader =  " "
+
+-- Modes
+--  normal = "n"
+--  insert = "i"
+--  visual = "v"
+--  visual block = "x"
+--  term = "t"
+--  command = "c"
+
+-- Normal --
+-- see :help normal-index for a full list of commands
+-- see :nmap
+
+keymap("n", "<leader>e", ":Lex 30<CR>", opts)
+
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<A-l>", ":bnext<CR>", opts)
+keymap("n", "<A-h>", ":bprevious<CR>", opts)
+
 -- Insert --
+-- see :help insert-index for a full list of commands
+-- see :imap
 keymap("i", "jk", "<ESC>", opts)  -- exit insert with jk
+
+-- Visual --
+-- see :help visual-index for a full list of commands
+-- see :vmap
+
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>=", opts)
+
+-- keep the clipboard item when pasting over a word
+keymap("v", "p", '".dP', opts)
+
+-- Visual Block --
+-- see :xmap
+
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Terminal --
+
+-- Open in a split
+keymap("n", "<leader>t", ":split<CR>:term<CR>", opts)
