2ea2f3f750c18b8326f90a846ac2307a942a840c
TSoli <tariqsoliman2000@gmail.com>
2023-06-13 20:55:19 +1000

    Added some colorschemes

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -45,6 +45,12 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
 
+  -- Colorschemes
+  use "folke/tokyonight.nvim"
+  use({ 'rose-pine/neovim', as = 'rose-pine' })
+  use { "ellisonleao/gruvbox.nvim" }
+  use "rebelot/kanagawa.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
