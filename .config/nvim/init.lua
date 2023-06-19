3aef39b65ccb52e6bdf1174e6d99492095895530
TSoli <tariqsoliman2000@gmail.com>
2023-06-19 15:37:52 +1000

    Changed from packer to lazy for plugin loading

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,6 @@
 require "user.options"
 require "user.keymaps"
-require "user.plugins"
+require "user.plugins" -- must map the leader key (in keymaps) before loading lazy
 require "user.colorscheme"
 require "user.cmp"
 require "user.lsp"
--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -1,100 +1,83 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
+-- Automatically install lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
     "git",
     "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd [[
-  augroup packer_user_config
+  augroup lazy_user_config
     autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    autocmd BufWritePost plugins.lua source <afile> | Lazy sync
   augroup end
 ]]
 
 -- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
+local status_ok, lazy = pcall(require, "lazy")
 if not status_ok then
+  vim.notify("Could not find lazy.")
   return
 end
 
--- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
-
 -- Install your plugins here
-return packer.startup(function(use)
+return lazy.setup({
   -- My plugins here
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  "nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
+  "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
 
   -- Colorschemes
-  use "folke/tokyonight.nvim"
-  use({ 'rose-pine/neovim', as = 'rose-pine' })
-  use { "ellisonleao/gruvbox.nvim" }
-  use "rebelot/kanagawa.nvim"
-  use "lunarvim/darkplus.nvim"
+  "folke/tokyonight.nvim",
+  { 'rose-pine/neovim', name = 'rose-pine' },
+  "ellisonleao/gruvbox.nvim",
+  "rebelot/kanagawa.nvim",
+  "lunarvim/darkplus.nvim",
 
   -- cmp plugins
-  use "hrsh7th/nvim-cmp" -- Completion plugin
-  use "hrsh7th/cmp-buffer" -- Buffer completions
-  use "hrsh7th/cmp-path" -- Path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "hrsh7th/cmp-nvim-lsp"
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  "hrsh7th/nvim-cmp", -- Completion plugin
+  "hrsh7th/cmp-buffer", -- Buffer completions
+  "hrsh7th/cmp-path", -- Path completions
+  "hrsh7th/cmp-cmdline", -- cmdline completions
+  "hrsh7th/cmp-nvim-lsp",
+  "saadparwaiz1/cmp_luasnip", -- snippet completions
 
   -- snippets
-  use "L3MON4D3/LuaSnip"  -- snippet engine
-  use "rafamadriz/friendly-snippets"  -- lots of snippets
+  "L3MON4D3/LuaSnip",  -- snippet engine
+  "rafamadriz/friendly-snippets",  -- lots of snippets
 
   -- LSP
-  use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/mason.nvim" -- simple to use language server installer
-  use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
-  use 'jose-elias-alvarez/null-ls.nvim' -- LSP diagnostics and code actions
+  "neovim/nvim-lspconfig", -- enable LSP
+  "williamboman/mason.nvim", -- simple to use language server installer
+  "williamboman/mason-lspconfig.nvim", -- simple to use language server installer
+  "jose-elias-alvarez/null-ls.nvim", -- LSP diagnostics and code actions
 
   -- Telescope (fuzzy finder)
-  use "nvim-telescope/telescope.nvim"
-  use "nvim-telescope/telescope-media-files.nvim"
+  "nvim-telescope/telescope.nvim",
+  "nvim-telescope/telescope-media-files.nvim",
 
   -- Treesitter
-  use {
+  {
     "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-  }
-  use "HiPhish/nvim-ts-rainbow2"  -- colour brackets
-  use "nvim-treesitter/playground"
-  use "windwp/nvim-autopairs" -- auto close brackets/quotes
-  use "windwp/nvim-ts-autotag"  -- autoclose/rename tags
-  use "RRethy/nvim-treesitter-endwise"  -- smart end in languages like lua
+    build = ":TSUpdate",
+  },
+  "HiPhish/nvim-ts-rainbow2",  -- colour brackets
+  "nvim-treesitter/playground",
+  "windwp/nvim-autopairs", -- auto close brackets/quotes
+  "windwp/nvim-ts-autotag",  -- autoclose/rename tags
+  "RRethy/nvim-treesitter-endwise",  -- smart end in languages like lua
 
   -- Markdown Previewer
-  use {
+  {
     "iamcco/markdown-preview.nvim",
-    run = "cd app && yarn install",
-    cmd = "MarkdownPreview",
-  }
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
+    build = "cd app && yarn install",
+    ft = "markdown",
+    -- cmd = "MarkdownPreview", -- Not working?
+  },
+})
