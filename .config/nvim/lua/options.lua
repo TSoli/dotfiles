a0919e9891a6bbc8899f40250cd600d1ecb2ebbd
TSoli <tariqsoliman2000@gmail.com>
2023-12-02 13:09:05 +1000

    Added spell checking autocomplete

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,51 +2,52 @@
 -- use :help options for details
 local configdir = vim.fn.stdpath("config")
 local options = {
-  backup = false,                                 -- save backups
-  backupdir = configdir .. "/.backup//",          -- backup location
-  clipboard = "",                                 -- use system clipboard
-  cmdheight = 2,                                  -- command line height
-  colorcolumn = { "80" },                         -- set vertical rule
-  completeopt = { "menu", "menuone", "preview" }, -- autocomplete options
-  conceallevel = 0,                               -- show concealed text
-  cursorline = true,                              -- highlight current line
-  cursorlineopt = "number",                       -- highlight current line number
-  dir = configdir .. "/.swp//",                   -- swap file directory
-  expandtab = true,                               -- make tabs spaces
-  exrc = true,
-  fileencoding = "utf-8",                         -- encoding applied to files when writte
-  hlsearch = true,                                -- search highlighting
-  incsearch = true,                               -- show where the pattern as it is typed
-  ignorecase = true,                              -- ignore case when searching
-  linebreak = true,                               -- do not break lines mid word
-  mouse = "a",                                    -- allow mouse in modes (n - normal, v - visual, i - insert, a - all see options for more)
-  number = true,                                  -- add line numbers
-  numberwidth = 2,                                -- set min number column width
-  -- pumblend = 20,                                  -- set pop up menu opacity
-  pumheight = 8,                                  -- pop up menu height for autocomplete
-  relativenumber = true,                          -- display relative line numbers
-  scrolloff = 8,                                  -- min num of lines above/below cursor
-  shiftwidth = 2,                                 -- number of spaces for autoindent
-  showmode = false,                               -- show the current mode
-  showtabline = 2,                                -- always show page tabs
-  sidescrolloff = 4,                              -- min num of cols either side if wrap is off
-  signcolumn = "auto",                            -- whether to show the signcolumn (i.e for breakpoints)
-  smartcase = true,                               -- override ignorecase if there is a capital
-  splitbelow = true,                              -- horizontal split below
-  splitright = true,                              -- vertical split right
-  swapfile = true,                                -- enable swap files
-  termguicolors = true,                           -- enable 24-bit RBG color in TUI
-  timeoutlen = 300,                               -- time to wait for mapped sequence to complete (in ms)
-  tabstop = 2,                                    -- number of spaces used to represent a <Tab> in a file
-  updatetime = 200,                               -- After this many ms of no typing write swap file to disk
-  undofile = true,                                -- enable undofiles
-  undodir = configdir .. "/.undo//",              -- undofile directory
-  wrap = false,                                   -- whether lines wrap
-  writebackup = true,                             -- enable backup before writing
+	backup = false, -- save backups
+	backupdir = configdir .. "/.backup//", -- backup location
+	clipboard = "", -- use system clipboard
+	cmdheight = 2, -- command line height
+	colorcolumn = { "80" }, -- set vertical rule
+	completeopt = { "menu", "menuone", "preview" }, -- autocomplete options
+	conceallevel = 0, -- show concealed text
+	cursorline = true, -- highlight current line
+	cursorlineopt = "number", -- highlight current line number
+	dir = configdir .. "/.swp//", -- swap file directory
+	expandtab = true, -- make tabs spaces
+	exrc = true,
+	fileencoding = "utf-8", -- encoding applied to files when writte
+	hlsearch = true, -- search highlighting
+	incsearch = true, -- show where the pattern as it is typed
+	ignorecase = true, -- ignore case when searching
+	linebreak = true, -- do not break lines mid word
+	mouse = "a", -- allow mouse in modes (n - normal, v - visual, i - insert, a - all see options for more)
+	number = true, -- add line numbers
+	numberwidth = 2, -- set min number column width
+	pumheight = 8, -- pop up menu height for autocomplete
+	relativenumber = true, -- display relative line numbers
+	scrolloff = 8, -- min num of lines above/below cursor
+	shiftwidth = 2, -- number of spaces for autoindent
+	showmode = false, -- show the current mode
+	showtabline = 2, -- always show page tabs
+	sidescrolloff = 4, -- min num of cols either side if wrap is off
+	signcolumn = "auto", -- whether to show the signcolumn (i.e for breakpoints)
+	smartcase = true, -- override ignorecase if there is a capital
+	spell = true,
+	spelllang = "en_au",
+	splitbelow = true, -- horizontal split below
+	splitright = true, -- vertical split right
+	swapfile = true, -- enable swap files
+	termguicolors = true, -- enable 24-bit RBG color in TUI
+	timeoutlen = 300, -- time to wait for mapped sequence to complete (in ms)
+	tabstop = 2, -- number of spaces used to represent a <Tab> in a file
+	updatetime = 200, -- After this many ms of no typing write swap file to disk
+	undofile = true, -- enable undofiles
+	undodir = configdir .. "/.undo//", -- undofile directory
+	wrap = false, -- whether lines wrap
+	writebackup = true, -- enable backup before writing
 }
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+	vim.opt[k] = v
 end
 
 -- Disable the default markdown style
@@ -54,4 +55,3 @@ vim.g.markdown_recommended_style = 0
 
 -- default colour change in colorscheme
 vim.cmd("hi ColorColumn guibg=#1FDBE2")
-
--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,150 +1,161 @@
 -- Completion plugins
 local M = {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "saadparwaiz1/cmp_luasnip",           -- snippet completions
-    "hrsh7th/cmp-nvim-lua",
-    {
-      "L3MON4D3/LuaSnip",                 -- snippet engine
-      event = "InsertEnter",
-      dependencies = {
-        "rafamadriz/friendly-snippets",   -- lots of snippets
-      },
-    },
-  },
-  event = {
-    "InsertEnter",
-    "CmdlineEnter",
-  },
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"saadparwaiz1/cmp_luasnip", -- snippet completions
+		"hrsh7th/cmp-nvim-lua",
+		"f3fora/cmp-spell",
+		{
+			"L3MON4D3/LuaSnip", -- snippet engine
+			event = "InsertEnter",
+			dependencies = {
+				"rafamadriz/friendly-snippets", -- lots of snippets
+			},
+		},
+	},
+	event = {
+		"InsertEnter",
+		"CmdlineEnter",
+	},
 }
 
 function M.config()
-  local cmp = require "cmp"
-  local luasnip = require "luasnip"
-  require("luasnip/loaders/from_vscode").lazy_load()
+	local cmp = require("cmp")
+	local luasnip = require("luasnip")
+	require("luasnip/loaders/from_vscode").lazy_load()
 
-  local check_backspace = function()
-    local col = vim.fn.col "." - 1
-    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
-  end
+	local check_backspace = function()
+		local col = vim.fn.col(".") - 1
+		return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+	end
 
-  local kind_icons = {
-    Text = "󰉿",
-    Method = "m",
-    Function = "󰊕",
-    Constructor = "",
-    Field = "",
-    Variable = "󰫧",
-    Class = "󰌗",
-    Interface = "",
-    Module = "",
-    Property = "",
-    Unit = "",
-    Value = "󰎠",
-    Enum = "",
-    Keyword = "󰌋",
-    Snippet = "",
-    Color = "󰏘",
-    File = "󰈙",
-    Reference = "",
-    Folder = "󰉋",
-    EnumMember = "",
-    Constant = "󰇽",
-    Struct = "",
-    Event = "",
-    Operator = "󰆕",
-    TypeParameter = "󰊄",
-    Codeium = "󰚩",
-    Copilot = "",
-  }
-  -- find more here: https://www.nerdfonts.com/cheat-sheet
+	local kind_icons = {
+		Text = "󰉿",
+		Method = "m",
+		Function = "󰊕",
+		Constructor = "",
+		Field = "",
+		Variable = "󰫧",
+		Class = "󰌗",
+		Interface = "",
+		Module = "",
+		Property = "",
+		Unit = "",
+		Value = "󰎠",
+		Enum = "",
+		Keyword = "󰌋",
+		Snippet = "",
+		Color = "󰏘",
+		File = "󰈙",
+		Reference = "",
+		Folder = "󰉋",
+		EnumMember = "",
+		Constant = "󰇽",
+		Struct = "",
+		Event = "",
+		Operator = "󰆕",
+		TypeParameter = "󰊄",
+		Codeium = "󰚩",
+		Copilot = "",
+	}
+	-- find more here: https://www.nerdfonts.com/cheat-sheet
 
-  cmp.setup {
-    snippet = {
-      expand = function(args)
-        luasnip.lsp_expand(args.body) -- For `luasnip` users.
-      end,
-    },
-    mapping = cmp.mapping.preset.insert {
-      ["<C-k>"] = cmp.mapping.select_prev_item(),
-      ["<C-j>"] = cmp.mapping.select_next_item(),
-      ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-      ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-      ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-      ["<C-e>"] = cmp.mapping {
-        i = cmp.mapping.abort(),
-        c = cmp.mapping.close(),
-      },
-      -- Accept currently selected item. If none selected, `select` first item.
-      -- Set `select` to `false` to only confirm explicitly selected items.
-      ["<CR>"] = cmp.mapping.confirm { select = true },
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expandable() then
-          luasnip.expand()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif check_backspace() then
-          fallback()
-        else
-          fallback()
-        end
-      end, {
-        "i",
-        "s",
-      }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, {
-        "i",
-        "s",
-      }),
-    },
-    formatting = {
-      fields = { "kind", "abbr", "menu" },
-      format = function(entry, vim_item)
-        vim_item.kind = kind_icons[vim_item.kind]
-        vim_item.menu = ({
-          nvim_lsp = "[LSP]",
-          nvim_lua = "[NVIM_LSP]",
-          luasnip = "{Snippet}",
-          buffer = "[Buffer]",
-          path = "[Path]",
-          emoji = "[Emoji]",
-        })[entry.source.name]
-        return vim_item
-      end,
-    },
-    sources = {
-      { name = "nvim_lsp" },
-      { name = "nvim_lua" },
-      { name = "luasnip" },
-      { name = "buffer" },
-      { name = "path" },
-    },
-    confirm_opts = {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = false,
-    },
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
-    experimental = {
-      ghost_text = true,
-    },
-  }
+	cmp.setup({
+		snippet = {
+			expand = function(args)
+				luasnip.lsp_expand(args.body) -- For `luasnip` users.
+			end,
+		},
+		mapping = cmp.mapping.preset.insert({
+			["<C-k>"] = cmp.mapping.select_prev_item(),
+			["<C-j>"] = cmp.mapping.select_next_item(),
+			["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+			["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+			["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+			["<C-e>"] = cmp.mapping({
+				i = cmp.mapping.abort(),
+				c = cmp.mapping.close(),
+			}),
+			-- Accept currently selected item. If none selected, `select` first item.
+			-- Set `select` to `false` to only confirm explicitly selected items.
+			["<CR>"] = cmp.mapping.confirm({ select = true }),
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif luasnip.expandable() then
+					luasnip.expand()
+				elseif luasnip.expand_or_jumpable() then
+					luasnip.expand_or_jump()
+				elseif check_backspace() then
+					fallback()
+				else
+					fallback()
+				end
+			end, {
+				"i",
+				"s",
+			}),
+			["<S-Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif luasnip.jumpable(-1) then
+					luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end, {
+				"i",
+				"s",
+			}),
+		}),
+		formatting = {
+			fields = { "kind", "abbr", "menu" },
+			format = function(entry, vim_item)
+				vim_item.kind = kind_icons[vim_item.kind]
+				vim_item.menu = ({
+					nvim_lsp = "[LSP]",
+					nvim_lua = "[NVIM_LSP]",
+					luasnip = "{Snippet}",
+					buffer = "[Buffer]",
+					path = "[Path]",
+					emoji = "[Emoji]",
+				})[entry.source.name]
+				return vim_item
+			end,
+		},
+		sources = {
+			{ name = "nvim_lsp" },
+			{ name = "nvim_lua" },
+			{ name = "luasnip" },
+			{ name = "buffer" },
+			{ name = "path" },
+			{
+				name = "spell",
+				option = {
+					keep_all_entries = false,
+					enable_in_context = function()
+            local ft = vim.bo.filetype
+						return require("cmp.config.context").in_treesitter_capture("spell") or ft == "tex" or ft == "sty"
+					end,
+				},
+			},
+		},
+		confirm_opts = {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = false,
+		},
+		window = {
+			completion = cmp.config.window.bordered(),
+			documentation = cmp.config.window.bordered(),
+		},
+		experimental = {
+			ghost_text = true,
+		},
+	})
 end
 
 return M
