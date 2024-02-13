fc301919dca032f59d3aa4fbf88453f3fa690f34
TSoli <tariqsoliman2000@gmail.com>
2024-02-14 09:11:02 +1000

    Updated polybar to show on all monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
+# If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 killall -q polybar
@@ -11,6 +11,13 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar example 2>&1 | tee -a /tmp/polybar1.log &
+# disown
 
-echo "Bars launched..."
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar --reload example &
+	done
+else
+	polybar --reload example &
+fi
