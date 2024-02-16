9fa2f27d48255b3a99e8e6d35d091205bc63c1c2
TSoli <tariqsoliman2000@gmail.com>
2024-02-16 10:13:33 +1000

    Added latexindent config

--- .setup_scripts/debian_based/packages/texlive.sh
+++ .setup_scripts/debian_based/packages/texlive.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get install texlive-full zathura
+
+# set up the indentconfig file
+
+sed "s@\$HOME@$HOME@" <~/.config/tex/indentconfig-template.yaml >~/.indentconfig.yaml
